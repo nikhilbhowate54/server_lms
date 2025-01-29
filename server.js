b60cfabb7d9b787f519bff500bc76d60fa96e63b
stellerNikhil <nikhil@stellarflux.com>
2025-01-29 16:45:02 +0530

    gitignore file added

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const multer = require("multer");
 var cors = require("cors");
 require("./db");
 const appRoutes = require("./routes");
+const PORT = process.env.PORT || 5000;
+const dotenv = require("dotenv");
+
+// dot env config
+dotenv.config();
 
 app.use("/uploads", express.static("uploads"));
 app.use(bodyParser.urlencoded());
@@ -20,6 +25,6 @@ app.use("/api", appRoutes);
 //   });
 // });
 
-app.listen(5000, (req, res) => {
+app.listen(PORT, (req, res) => {
   console.log("Server is listening on port 5000");
 });
